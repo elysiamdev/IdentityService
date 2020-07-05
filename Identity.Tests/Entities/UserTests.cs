34ba6ea2b984230a33add7a976dec9a0d9096a2b
Lucas <lucasreispedroso@gmail.com>
2020-07-05 00:21:45 -0300

    tests(user): first user test

--- Identity.Tests/Entities/UserTests.cs
+++ Identity.Tests/Entities/UserTests.cs
@@ -1,4 +1,7 @@
 using System;
+using Identity.Domain.Entities;
+using Identity.Domain.ValueObjects;
+
 using Xunit;
 
 
@@ -6,6 +9,13 @@ namespace Identity.Tests.Entities
 {
     public class UserTests
     {
-
+        public void TestUser()
+        {
+            var name = new Name("Lucas", "Pedroso");
+            var email = new Email("lucasreisPedroso@gmail.com");
+            var password = new Password("1029301293");
+            User user = new User(1, name, email, password);
+            
+        }
     }
 }
