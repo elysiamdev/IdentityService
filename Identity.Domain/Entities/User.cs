e07f9ef0431b44797fe7b6680789b9b7b5a0e0c9
Lucas <lucasreispedroso@gmail.com>
2020-07-04 23:04:26 -0300

    feat(user): constructor

--- Identity.Domain/Entities/User.cs
+++ Identity.Domain/Entities/User.cs
@@ -5,9 +5,17 @@ namespace Identity.Domain.Entities
 {
     public class User : Entity
     {
-        public int UserId { get; set; }
-        public Name Name { get; set; }
-        public Email Email { get; set; }
-        public Password Password { get; set; }
+        public User(int userId, Name name, Email email, Password password)
+        {
+            UserId = userId;
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public int UserId { get; private set; }
+        public Name Name { get; private set; }
+        public Email Email { get; private set; }
+        public Password Password { get; private set; }
     }
 }
